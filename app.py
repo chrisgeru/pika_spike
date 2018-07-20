b05e5cab878cef94115371ed8c1c9f4ae72fa29a
Chris Maillefaud <christian@geru.com.br>
2018-07-19 23:02:49 -0300

    Add jinja template and model

--- app.py
+++ app.py
@@ -1,13 +1,40 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
 
+PROJECT_CONFIGURATION = {
+    'user': {
+        'mongo_uri': "mongodb://mongo_user:27017/user"
+    },
+    'score': {
+        'mongo_uri': "mongodb://mongo_score:27017/score"
+    },
+    'money': {
+        'mongo_uri': "mongodb://mongo_money:27017/money"
+    }
+}
+project_name = os.getenv('PROJECT_NAME')
+config = PROJECT_CONFIGURATION[project_name]
 app = Flask(__name__)
-
+app.config["MONGO_URI"] = config['mongo_uri']
+mongo = PyMongo(app)
 
 @app.route('/')
 def hello_world():
-    return f"Hello World for Project {os.getenv('PROJECT_NAME')}!"
+    if not mongo.db[project_name].find_one():
+        initial_data = {
+            'user': '',
+            'score': '',
+            'money': 0
+        }
+        mongo.db[project_name].insert_one(initial_data)
+    record = mongo.db[project_name].find_one()
+    data = {
+        'name': project_name,
+        'record': record
+    }
+    return render_template("index.html", **data)
 
 
 if __name__ == '__main__':
