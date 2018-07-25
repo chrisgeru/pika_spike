3ceca06f903dcdbde9acd6658803388a57293aa5
Chris Maillefaud <christian@geru.com.br>
2018-07-24 22:30:40 -0300

    Add jinja template and model

--- app.py
+++ app.py
@@ -3,33 +3,34 @@ import os
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
 
-PROJECT_CONFIGURATION = {
-    'user': {
-        'mongo_uri': "mongodb://mongo_user:27017/user"
-    },
-    'score': {
-        'mongo_uri': "mongodb://mongo_score:27017/score"
-    },
-    'money': {
-        'mongo_uri': "mongodb://mongo_money:27017/money"
-    }
-}
 project_name = os.getenv('PROJECT_NAME')
-config = PROJECT_CONFIGURATION[project_name]
 app = Flask(__name__)
-app.config["MONGO_URI"] = config['mongo_uri']
+app.config["MONGO_URI"] = f"mongodb://mongo_{project_name}:27017/{project_name}"
 mongo = PyMongo(app)
 
-@app.route('/')
-def hello_world():
-    if not mongo.db[project_name].find_one():
+
+def get_record(name):
+    if not mongo.db[name].find_one():
         initial_data = {
             'user': '',
             'score': '',
             'money': 0
         }
-        mongo.db[project_name].insert_one(initial_data)
-    record = mongo.db[project_name].find_one()
+        mongo.db[name].insert_one(initial_data)
+    record = mongo.db[name].find_one()
+    return record
+
+
+@app.route('/user')
+@app.route('/score')
+@app.route('/money')
+def publisher():
+    return "Start publishing"
+
+
+@app.route('/')
+def index():
+    record = get_record(project_name)
     data = {
         'name': project_name,
         'record': record
